8372842f578515ca68774fe311b90be04e520f8f
mohitmasiwal <mohitmasiwal02@gmail.com>
2025-04-03 14:19:10 +0530

    new

--- crud.js
+++ crud.js
@@ -1,15 +1,13 @@
 function handleSubmit(event) {
     event.preventDefault();
     
-    let price = document.getElementById("price").value;
+    let price = parseFloat(document.getElementById("price").value);  
     let productName = document.getElementById("productName").value;
-
     let obj = { price, productName };
 
     axios.post("https://crudcrud.com/api/44af3b836dc94c6d88c158028d2e2392/prisedetail", obj)
         .then((res) => {
-            console.log(res);
-            getuser();  
+            getuser();   
         })
         .catch((err) => console.log(err));
 }
@@ -18,6 +16,7 @@ function getuser() {
     axios.get("https://crudcrud.com/api/44af3b836dc94c6d88c158028d2e2392/prisedetail")
         .then((res) => {
             display(res.data);
+            calculateTotal(res.data);  
         })
         .catch((err) => console.log(err));
 }
@@ -32,7 +31,7 @@ function display(users) {
 
         del.innerHTML = "Delete Product";
         del.addEventListener("click", () => {
-            deleteproduct(ele._id);
+            deleteproduct(ele._id, ele.price);   
         });
 
         listitem.innerHTML = `Price: ${ele.price}, Product: ${ele.productName}`;
@@ -41,10 +40,18 @@ function display(users) {
     });
 }
 
-function deleteproduct(id) {
+function deleteproduct(id, price) {
     axios.delete(`https://crudcrud.com/api/44af3b836dc94c6d88c158028d2e2392/prisedetail/${id}`)
-        .then(() => getuser())  // ✅ Fix: Fetch updated list after deletion
+        .then(() => {
+            getuser();   
+        })
         .catch(err => console.log(err));
+}
+
+function calculateTotal(users) {
+    let totalValue = document.getElementById("totalValue");
+    let totalPrice = users.reduce((sum, item) => sum + parseFloat(item.price), 0);  
+    totalValue.innerHTML = `Total Price: ${totalPrice}`;
 }
 
  
